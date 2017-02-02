6719c48c859b693437233136b3b469caab5f2e65
Xavier Maldonado <xavieramaldonado@lewisu.edu>
2017-02-02 17:29:13 +0000

    mid class commit

--- first.rb
+++ first.rb
@@ -1,3 +1,13 @@
 print "Enter your name: "
 name = gets.chomp
+puts "Hello #{name}."
+puts name.upcase
+puts name.downcase
+puts name.capitalize
 puts name
+
+name = name.split(" ")
+first = name[0]
+last = name[1]
+puts first
+puts last
